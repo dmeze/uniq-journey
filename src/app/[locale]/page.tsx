d2435b13681c5d9a6786b3417b21a984d9418028
Danylo Mezentsev <dmezents@cisco.com>
2024-01-19 01:50:01 +0200

    dmezents: Fix eslint

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,4 @@
-import { useTranslations } from 'next-intl'
-
 const Home = () => {
-  const t = useTranslations('Home')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24" />
   )
