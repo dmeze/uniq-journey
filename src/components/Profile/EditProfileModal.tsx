9f59f780c6cd809bfca9588ea560da97a21d027a
Danylo Mezentsev <dmezents@cisco.com>
2024-06-03 23:52:57 +0200

    [RAND-1]: dmeze: Add User Edit Flow
    https://jira-eng-rtp3.cisco.com/jira/browse/RAND-1

--- src/components/Profile/EditProfileModal.tsx
+++ src/components/Profile/EditProfileModal.tsx
@@ -1,81 +1,80 @@
-import type { ChangeEvent, FormEvent } from 'react'
 import React, { useState } from 'react'
 import type { User } from '@prisma/client'
 
 import { getCityRef, getWarehouseOptions } from '@/components/Checkout/helpers'
 import type { IOption } from '@/components/Select/ActionSelect'
 import ActionSelect from '@/components/Select/ActionSelect'
+import { userFields } from '@/components/Profile/constants'
+import { updateUser } from '@/app/actions/user/actions'
+import type { UserUpdateData } from '@/app/actions/user/actions'
 
 import Modal from '../Modal'
 
+export interface UserWithKey extends User {
+  [key: string]: string | null
+}
+
 interface EditProfileModalProps {
-  user: User
+  user: UserWithKey
   isOpen: boolean
   onClose: () => void
-  onSave: (updatedUser: User) => void
 }
 
 const EditProfileModal: React.FC<EditProfileModalProps> = ({
   user,
   isOpen,
   onClose,
-  onSave,
 }) => {
-  const [formData, setFormData] = useState<User>({ ...user })
   const [city, setCity] = useState({ value: '', label: user.city })
-  const [warehouse, setWarehouse] = useState('')
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setFormData((prev) => ({ ...prev, [name]: value }))
-  }
+  const [warehouse, setWarehouse] = useState(user.warehouse)
 
   const handleCityChange = (option: IOption) => {
     setWarehouse('')
     setCity(option)
-    setFormData((prev) => ({ ...prev, city: option.label }))
   }
 
   const handleWarehouseChange = (option: IOption) => {
     setWarehouse(option.label)
-    setFormData((prev) => ({ ...prev, warehouse: option.label }))
   }
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
-    onSave(formData)
-  }
+  const handleSubmit = async (formData: FormData) => {
+    onClose()
 
-  const inputsArray = [
-    { label: 'Name', type: 'text', name: 'name', value: formData.name },
-    { label: 'Email', type: 'email', name: 'email', value: formData.email },
-    { label: 'Phone', type: 'text', name: 'phone', value: formData.phone },
-  ]
+    const userData = {
+      ...Object.fromEntries(formData),
+      city: city.label,
+      warehouse,
+    } as UserUpdateData
+
+    await updateUser(userData)
+  }
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} title="Edit Profile">
-      <form onSubmit={handleSubmit}>
-        {inputsArray.map((input) => (
-          <div className="mb-4" key={input.name}>
-            <label htmlFor={input.name} className="block text-gray-700">
-              {input.label}
-            </label>
-            <input
-              id={input.name}
-              type={input.type}
-              name={input.name}
-              value={input.value || ''}
-              onChange={handleChange}
-              className="mt-1 w-full rounded border p-2"
-            />
-          </div>
-        ))}
+      <form action={handleSubmit}>
+        {userFields.map(
+          ({ name, label, type }) =>
+            type && (
+              <div className="mb-4" key={name}>
+                <label htmlFor={name} className="block text-gray-700">
+                  {label}
+                </label>
+                <input
+                  id={name}
+                  type={type}
+                  name={name}
+                  defaultValue={user[name] || ''}
+                  className="mt-1 w-full rounded border p-2"
+                />
+              </div>
+            ),
+        )}
         <ActionSelect
           setSelectedValue={handleCityChange}
           id="city"
           label="City"
           fetchOptions={getCityRef}
-          defaultValue={city.label}
+          defaultValue={user.city}
         />
         <ActionSelect
           setSelectedValue={handleWarehouseChange}
@@ -83,19 +82,26 @@ const EditProfileModal: React.FC<EditProfileModalProps> = ({
           label="Warehouse"
           fetchOptions={(value) => getWarehouseOptions(city.value, value)}
           refreshValue={city.value}
-          defaultValue={warehouse}
+          defaultValue={user.warehouse}
         />
         <div className="flex justify-end">
           <button
             type="button"
             onClick={onClose}
-            className="mr-4 rounded bg-gray-500 px-6 py-2 text-white transition duration-300 hover:bg-gray-700"
+            className="mr-4 rounded-lg bg-transparent px-5 py-2 text-dark-green-500 transition duration-300 hover:border hover:border-light-green-500 hover:bg-light-green-600 hover:text-dark-green-900"
           >
             Cancel
           </button>
           <button
             type="submit"
-            className="rounded bg-green-500 px-6 py-2 text-white transition duration-300 hover:bg-green-700"
+            className="
+                  rounded-lg border-2 border-light-green
+                  bg-light-green
+                  px-6 py-2
+                  font-bold text-white
+                  transition duration-300 hover:-translate-y-1 hover:shadow-lg
+                  disabled:opacity-70
+                "
           >
             Save
           </button>
---
+++ src/components/Profile/constants.ts
@@ -0,0 +1,7 @@
+export const userFields = [
+  { label: 'Name', type: 'text', name: 'name' },
+  { label: 'Email', type: 'email', name: 'email' },
+  { label: 'Phone', type: 'text', name: 'phone' },
+  { label: 'City', name: 'city' },
+  { label: 'Warehouse', name: 'warehouse' },
+]
