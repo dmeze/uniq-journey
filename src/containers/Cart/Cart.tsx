d24b507db35c5bb166074174d7ed0bbf4324ffdd
Danylo Mezentsev <dmezents@cisco.com>
2024-06-07 03:30:47 +0200

    [RAND-1]: dmeze: Fix types
    https://jira-eng-rtp3.cisco.com/jira/browse/RAND-1

--- src/containers/Cart/Cart.tsx
+++ src/containers/Cart/Cart.tsx
@@ -1,9 +1,10 @@
 import CartWrapper from '@/components/Cart/CartWrapper'
 import CartBody from '@/components/Cart/CartBody'
 import { getCart } from '@/app/actions/cart/actions'
+import type { CartData } from '@/components/Cart'
 
 const Cart = async () => {
-  const cartData = await getCart()
+  const cartData = (await getCart()) as unknown as CartData
 
   return (
     <CartWrapper cartId={cartData.id!} cartCount={cartData.count!}>
